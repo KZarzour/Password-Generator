180924b5ddcf94c24edf53feae8b84647bd4d36c
Michal Malinowski <malinowski.michalpl@gmail.com>
2022-07-06 11:29:11 +0200

    Small changes in charType method

--- Password Generator/src/Password.java
+++ Password Generator/src/Password.java
@@ -12,7 +12,8 @@ public class Password {
         int val;
 
         // Char is Uppercase Letter
-        if ((int) C >= 65 && (int) C <= 90) val = 1;
+        if ((int) C >= 65 && (int) C <= 90)
+            val = 1;
 
         // Char is Lowercase Letter
         else if ((int) C >= 97 && (int) C <= 122) {
@@ -30,7 +31,6 @@ public class Password {
         }
 
         return val;
-
     }
 
     public int PasswordStrength() {
