4757c0d8fe0aab43ba0e140353e87e866fc2d3f5
Mahendra <mahendradev3699@gmail.com>
2023-12-14 19:40:33 +0530

    We are removing char line

--- Password Generator/src/Alphabet.java
+++ Password Generator/src/Alphabet.java
@@ -5,7 +5,8 @@ public class Alphabet {
 	
 	public static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
-	public static final String NUMBERS = "1234567890";
+	//git add .
+	// public static final String NUMBERS = "1234567890";
 	public static final String SYMBOLS = "!@#$%^&*()-_=+\\/~?";
 	
 	private final StringBuilder pool;
